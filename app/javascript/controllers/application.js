ededf593074b974c6e7aa6a31446a3276d9a78c3
Sasa Kocic <sasa.kocic@gmail.com>
2022-10-12 06:02:17 +0200

    ready for production

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,15 @@
 import { Application } from "@hotwired/stimulus"
+import "@hotwired/turbo-rails"
+import "../controllers"
+import * as bootstrap from "bootstrap"
 
 const application = Application.start()
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
 
 // Configure Stimulus development experience
 application.debug = false
--- db/migrate/20221011212856_create_authors.rb
+++ db/migrate/20221011212856_create_authors.rb
@@ -2,6 +2,11 @@
 
 class CreateAuthors < ActiveRecord::Migration[7.0]
   def change
-    create_table :authors, &:timestamps
+    create_table :authors do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.timestamps null: false
+    end
   end
 end
